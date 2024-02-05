f629d124e9aeca4c59398b787a199ad50a6ca26e
pouya heydarabadi <mry49hi@gmail.com>
2024-02-05 16:01:58 +0330

    feat: Add String&Int Example in Variable Chapter

---
+++ C#/01/Variable/SampleVar.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_._01.Variable;
+
+public class SampleVar
+{
+    public void Main()
+    {
+        // Definition String Variable
+        string str1 = new string("string1");
+        string str2 = new("string2");
+        string str3 = "string3";
+
+        // Definition Int Variable
+        int int1 = 1;
+        int int2 = 2;
+
+        int intMin = int.MinValue; // -2147483648
+        int intMax = int.MaxValue; // 2147483647
+
+        Console.WriteLine($"Int Min value : {intMin} -- Int Max Value : {intMax}");
+    }
+}
--- C#/Program.cs
+++ C#/Program.cs
@@ -1,1 +1,4 @@
-﻿
+﻿using C_._01.Variable;
+
+SampleVar example = new();
+example.Main();
