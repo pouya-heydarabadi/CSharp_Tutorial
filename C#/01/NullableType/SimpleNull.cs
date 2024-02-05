a89f464b261fcd55fc0a325b4db00f8552f05051
pouya heydarabadi <mry49hi@gmail.com>
2024-02-05 23:17:10 +0330

    feat: Add Exmple for Nullable type & Handler NullException in develope cycle Example

---
+++ C#/01/NullableType/SimpleNull.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.InteropServices.ObjectiveC;
+
+namespace CSharp._01.NullableType;
+
+public class SimpleNull
+{
+    public void main()
+    {
+
+        // Definition Nullable String 
+        string name = null;
+        string? nullableName = null;
+
+        // Handle by If Stetements
+        if (name is string || !string.IsNullOrWhiteSpace(name))
+            Console.WriteLine(name.ToString() + "name");
+
+        Console.WriteLine(nullableName + "nullableName");
+
+        // Error Handeling in Nullable Value Objects
+        object _obj1 = null;
+        object obj2 = _obj1 ?? throw new NullReferenceException("Value Cannot Be Null");
+
+    }
+}
