6ad9024a715030ea9804f96ef61e7d4efb589b3a
pouya heydarabadi <mry49hi@gmail.com>
2024-02-11 17:56:15 +0330

    feat: Add Entity DayOfWeek Class

---
+++ 02/02/Enums/DayOfWeek.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02._02.Enums;
+
+public class DayOfWeek
+{
+    public int Id { get; set; }
+    public DayOfWeekEnumByte DayOfWeeks { get; set; }
+}
