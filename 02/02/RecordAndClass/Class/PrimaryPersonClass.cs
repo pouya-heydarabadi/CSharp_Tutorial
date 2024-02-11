bc66032e7e89a5ec559eb8a4302e1f93d533580e
s3cn3t1 <159703265+s3cn3t1@users.noreply.github.com>
2024-02-11 17:20:21 +0330

    Update PrimaryPersonClass.cs
    please Add Age Behavior In Program

--- 02/02/RecordAndClass/Class/PrimaryPersonClass.cs
+++ 02/02/RecordAndClass/Class/PrimaryPersonClass.cs
@@ -1,4 +1,4 @@
 ﻿namespace CSharp._02.RecordAndClass.Class;
 
 // Primary Constructors
-public class PrimaryPersonClass(int Id, string FirstName, string LastName);
+public class PrimaryPersonClass(int Id, string FirstName, string LastName, int Age);
