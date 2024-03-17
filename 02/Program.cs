a3973532d5b1cd32be1e43b7dc4394c6232a6483
Pouya Heydarabadi <heydarabadip@gmail.com>
2024-03-17 09:16:11 +0330

    style: Change Folder In Project

--- 02/Program.cs
+++ 02/Program.cs
@@ -63,4 +63,22 @@
 
 #endregion
 #endregion
+
+#region Generics
+using _05._05;
+//Console.WriteLine("Hello, World!");
+//GenericConcat<int> genericConcatInt = new GenericConcat<int>();
+//var result = genericConcatInt.Concat(1, 2);
+//Console.WriteLine(result);
+
+//GenericConcat<string> genericConcatString = new GenericConcat<string>();
+//var resultString = genericConcatString.Concat("Alireza", "Oroumand");
+//Console.WriteLine(resultString);
+
+
+//GenericConcat<Person> genericConcatPerson = new GenericConcat<Person>();
+
+//var resultPerson = genericConcatPerson.Concat(new Person { FirstName = "TestFirstName", LastName = "TesLastName" }, new Person { FirstName = "Arbab", LastName = "Arbabi" });
+//Console.WriteLine(resultPerson);
+#endregion
 Console.ReadLine();
--- 05/Program.cs
+++ 05/Program.cs
@@ -1,17 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using _05._05;
 
-Console.WriteLine("Hello, World!");
-GenericConcat<int>  genericConcatInt = new GenericConcat<int>();
-var result = genericConcatInt.Concat(1, 2);
-Console.WriteLine(result);
-
-GenericConcat<string> genericConcatString = new GenericConcat<string>();
-var resultString = genericConcatString.Concat("Alireza", "Oroumand");
-Console.WriteLine(resultString);
-
-
-GenericConcat<Person> genericConcatPerson = new GenericConcat<Person>();
-
-var resultPerson = genericConcatPerson.Concat( new Person { FirstName = "TestFirstName" , LastName = "TesLastName" }, new Person { FirstName="Arbab", LastName="Arbabi"});
-Console.WriteLine(resultPerson);
