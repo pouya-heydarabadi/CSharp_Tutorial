549695f2bb08e9ba61fca952e933e80f664681b3
Pouya Heydarabadi <heydarabadip@gmail.com>
2024-03-25 20:51:09 +0330

    feat: Add AnonymouseMethod Example

---
+++ 02/02/Delegates/AnonymouseMethods/AnonymousMethodExecute.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+using _02._02.Delegates.Delegate.Models;
+
+namespace _02._02.Delegates.AnonymouseMethods;
+
+public class AnonymousMethodExecute
+{
+    public void Execute()
+    {
+        // Init New Person
+        Person objNewPerson = new Person()
+        {
+            Id = Guid.NewGuid(),
+            FirstName = "pouya",
+            LastName = "heydarabadi"
+        };
+
+        var sample = delegate (Person person)
+        {
+            return $"{person.FirstName}";
+        };
+
+        var result = sample(objNewPerson);
+        Console.WriteLine(result);
+    }
+}
