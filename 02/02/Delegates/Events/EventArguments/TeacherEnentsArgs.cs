9f045935b85ed9a79cfad067d5d8a5d234d0804c
Pouya Heydarabadi <heydarabadip@gmail.com>
2024-03-25 21:14:54 +0330

    feat: Add TeacherEventArgs For Event

---
+++ 02/02/Delegates/Events/EventArguments/TeacherEnentsArgs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02._02.Delegates.Events.EventArguments;
+
+public class TeacherEnentsArgs:EventArgs
+{
+    public string OldName { get; set; }
+    public string NewName { get; set; }
+
+    public TeacherEnentsArgs(string oldName, string newName)
+    {
+        this.OldName = oldName;
+        this.NewName = newName;
+    }
+}
