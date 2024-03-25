0ba8c386914be945a13b7a9efb6cc1f5ffeee518
Pouya Heydarabadi <heydarabadip@gmail.com>
2024-03-25 20:09:04 +0330

    feat: add simple Action

---
+++ 02/02/Delegates/MultiCastDelegates/SampleActions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02._02.Delegates.MultiCastDelegates;
+
+public class SampleActions
+{
+    public void CW1()
+        => Console.WriteLine($"Hello {nameof(CW1)}");
+
+    public void CW2()
+        => Console.WriteLine($"Hello {nameof(CW2)}");
+
+    public void CW3()
+        => Console.WriteLine($"Hello {nameof(CW3)}");
+
+    public void CW4()
+        => Console.WriteLine($"Hello {nameof(CW4)}");
+}
