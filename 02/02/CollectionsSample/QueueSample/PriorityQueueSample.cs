770d9691a6de351476dd9626f0d25b5643fd5d8e
Pouya Heydarabadi <heydarabadip@gmail.com>
2024-03-26 21:59:28 +0330

    feat: Add PriiorityQueue Example

---
+++ 02/02/CollectionsSample/QueueSample/PriorityQueueSample.cs
@@ -0,0 +1,21 @@
+﻿namespace _02._02.CollectionsSample.QueueSample;
+
+public class PriorityQueueSample<E,P>
+{
+    public PriorityQueue<E, P> objPriorityQueue = new PriorityQueue<E, P>();
+
+    public void Add(E element, P priority)
+    {
+        objPriorityQueue.Enqueue(element, priority);
+    }
+
+    public object DeQueue()
+    {
+       return objPriorityQueue.Dequeue();
+    }
+
+    public object Peek()
+    {
+        return objPriorityQueue.Peek();
+    }
+}
