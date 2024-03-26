4d7a980ffa112262ac902dc8a5ffe0adb8766535
Pouya Heydarabadi <heydarabadip@gmail.com>
2024-03-26 21:59:07 +0330

    feat: Add Queue Example

---
+++ 02/02/CollectionsSample/QueueSample/QueueExecute.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02._02.CollectionsSample.QueueSample;
+
+public class QueueExecute<T>
+{
+    public Queue<T> objQueueSample1 = new Queue<T>();
+
+    public void EnQueue(T newData)
+    {
+        objQueueSample1.Enqueue(newData);
+    }
+
+    public T DeQueue()
+    {
+        return objQueueSample1.Dequeue();
+    }
+
+    public void WriteAllElements()
+    {
+        foreach (T item in objQueueSample1)
+        {
+            Console.WriteLine(item);
+        }
+    }
+
+    public void WriteCount()
+    {
+        Console.WriteLine($"Your Queue Count: {objQueueSample1.Count()}");
+    }
+
+    public T Peak()
+    {
+        return objQueueSample1.Peek();
+    }
+
+}
